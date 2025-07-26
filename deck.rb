d8912e787ce0151b27101795d7fe667a125b4c79
Megumi Kishibe <Kishibe0417@gmail.com>
2025-07-26 12:35:14 +0900

    Refactor Deckclass:simplized

--- deck.rb
+++ deck.rb
@@ -4,17 +4,24 @@ class Deck
     attr_accessor :cards #山札（配列）読み取れるようにする
 
     def initialize
-        @cards = create_deck #create_deckメソッド
+        @cards = []
+        suits = ["ハート", "スペード", "クラブ", "ダイヤ"]
+        ranks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+            suits.each do |suit|
+                ranks.each do |rank|
+                    @cards << Card.new(suit, rank)
+                end
+            end
+    end
+    def shuffle
         @cards.shuffle!
     end
 end
 
-private #ここから外から触ってはいけない内部処理を書きます
+#---TESTCODE---
+deck = Deck.new
 
-def create_deck
-    suits = ["ハート","ダイヤ","クラブ","スペード"]
-    ranks = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
-    suits.flat_map do |suit|
-        ranks.map {|rank| Card.new(suit,rank) }
-    end
-end
+#カードが５２枚生成されるか？
+puts "Total cards: #{deck.cards.size}"
+
+
