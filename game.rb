ef6d5e87fd418a1deb0da9623285bdd749f62f3b
Megumi Kishibe <Kishibe0417@gmail.com>
2025-07-27 10:45:37 +0900

    Finish Step2

--- game.rb
+++ game.rb
@@ -22,12 +22,14 @@ class Game
 
         deal_cards
 
-        2.times do
+        until @deck.cards.empty? #カードがなくなるまで繰り返す
             puts "戦争！"
             play_turn
         end
 
-     puts "戦争を終了します"
+        display_result
+
+        puts "戦争を終了します"
     end
 
     def play_turn
@@ -35,7 +37,7 @@ class Game
         @players.each do |player|
             player.card = @deck.cards.pop
         end
-        
+
         puts @players[0].show_card
         puts @players[1].show_card
 
@@ -43,10 +45,19 @@ class Game
             puts "引き分けです。"
         else
             winner = [@players[0], @players[1]].max_by { |p| rank_value(p.card.rank) }
-            puts "#{winner.name}が勝ちました"
+            puts "#{winner.name}が勝ちました。#{winner.name}はカードを2枚もらいました"
+            winner.won_cards << @players[0].card #勝ったプレイヤーにカードを渡す
+            winner.won_cards << @players[1].card
         end
     end
 
+    def display_result #カードの数を比べて結果表示
+        if @players[0].won_cards.size > @players[1].won_cards.size
+            puts "#{@players[0].name}が１位,#{@players[1].name}が２位です。"
+        else
+            puts "#{@players[1].name}が１位,#{@players[0].name}が２位です。"
+        end
+    end
     private
 
     def rank_value(rank)
--- player.rb
+++ player.rb
@@ -1,8 +1,9 @@
 class Player
-    attr_accessor :name, :card #We need ":card" so we can assign a card to the player later
+    attr_accessor :name, :card, :won_cards #We need ":card" so we can assign a card to the player later
     def initialize(name)
         @name = name
         @card = nil     #Start with no card assigned to the player
+        @won_cards = []
     end
     def show_card
         if @card
